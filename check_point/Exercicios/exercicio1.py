c90f816e58f6686a15e0d060b522a3add1f3d4e0
Wesley Kleim <80065371+wesleykleim@users.noreply.github.com>
2022-04-07 23:13:51 -0300

    Delete exercicio1.py

--- check_point/Exercicios/exercicio1.py
+++
@@ -1,9 +0,0 @@
-aulas_semanais = int(input("Quantidade de aulas semanais: "))
-hora_aula = float(input("Valor da hora aula: "))
-
-salario_base = aulas_semanais * 4.5 * hora_aula
-hora_atividade = salario_base * 5 / 100
-dsr = (salario_base + hora_atividade) / 6
-salario_mensal = salario_base + hora_atividade + dsr
-
-print("salario bas")
