3fa5a9e3340bbc5f0e21d3b1d0d2a586582c407a
Wesley Kleim <80065371+wesleykleim@users.noreply.github.com>
2022-04-07 23:11:22 -0300

    Delete lista3_11.py

--- Exercicios lista03/lista3_11.py
+++
@@ -1,6 +0,0 @@
-preco = float(input("informe o valor do produto "))
-escolha = int(input("Informe a forma de pagamento"))
-
-if  escolha == 1:
-    print("10% de desconto")
-    print("valor: {:.2f}")
