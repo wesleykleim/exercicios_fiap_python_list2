5cb33a16fd1c33994e2b3256a64b27d6c591bb0e
Wesley Kleim <80065371+wesleykleim@users.noreply.github.com>
2022-04-07 23:15:54 -0300

    Delete lista03/lista4 directory

--- lista03/lista4/problema46.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Digite num: "))
-
-divisor = 1
-
-while divisor <= num:
-    if num % divisor == 0:
-        soma = soma + divisor
-    divisor = divisor + 1
-print("A soma dos divisores é", soma)
