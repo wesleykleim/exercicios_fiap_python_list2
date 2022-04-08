f13c843c1e4494b3814cff2ccb12e52ced413928
Wesley Kleim <80065371+wesleykleim@users.noreply.github.com>
2022-04-07 23:14:29 -0300

    Delete auala_04_04_2021_python directory

--- auala_04_04_2021_python/lista3_5.py
+++
@@ -1,5 +0,0 @@
-dias_uteis = int(input("Dias úteis"))
-horas_trabalhadas = float(input("Quantidade de horas trabalhadas: "))
-valor_hora = float(input("Informe o valor da hora"))
-
-jornada = dias_uteis
